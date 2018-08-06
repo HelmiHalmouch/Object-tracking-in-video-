fbd8967d1a4b844ea36644a89f50f1d123f5fed5
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-08-06 23:21:52 +0200

    add general description of the main algorithme

---
+++ main_algorithme.py
@@ -0,0 +1,9 @@
+#main description 
+
+'''
+	Here is the main script for object tracking sing python and opencv
+	
+	You need to use Opencv3.4 and python3.x to run this code 
+
+	TODO: transfor this code using function and class in python
+'''
